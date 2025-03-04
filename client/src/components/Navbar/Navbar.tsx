64694f38825e2979c3f1a2c4e3fe79921c4862d0
Rajib <prorajib6@gmail.com>
2025-03-04 22:47:25 +0600

    design navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,17 @@
+'use client';
 import { assets } from '@/assets/frontend_assets/assets';
-import Image from 'next/image';
-import React from 'react';
 
+import Image from 'next/image';
+import React, { useState } from 'react';
+const menuItems = [
+  { name: 'Home', url: '/' },
+  { name: 'Menu', url: '/menu' },
+  { name: 'Mobile-app', url: '/mobile-app' },
+  { name: 'Contact us', url: '/contact-us' },
+];
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState('Home');
+
   return (
     <div className='container  mx-auto '>
       <div className='py-5 flex justify-between items-center  border-b-2 '>
@@ -19,23 +28,24 @@ const Navbar = () => {
         </div>
 
         {/* Navigation links - centered */}
-        <ul className='flex flex-row gap-4 justify-center space-x-8 py-10'>
-          <li className='cursor-pointer hover:text-orange-500 transition-colors'>
-            Home
-          </li>
-          <li className='cursor-pointer hover:text-orange-500 transition-colors'>
-            Menu
-          </li>
-          <li className='cursor-pointer hover:text-orange-500 transition-colors'>
-            Mobile-app
-          </li>
-          <li className='cursor-pointer hover:text-orange-500 transition-colors'>
-            Contact us
-          </li>
+        <ul className='text-[18px] flex flex-row text-gray-800 list-none gap-5 justify-center space-x-8 '>
+          {menuItems.map((item) => (
+            <li
+              key={item.name}
+              className={`cursor-pointer hover:text-orange-500 transition-colors ${
+                item.name === activeMenu
+                  ? 'text-orange-500 border-b-2 border-b-orange-500 transition-all'
+                  : ''
+              }`}
+              onClick={() => setActiveMenu(item.name)}
+            >
+              {item.name}
+            </li>
+          ))}
         </ul>
 
         {/* Right icons section */}
-        <div className='navbar-right flex-1 flex justify-end items-center space-x-4'>
+        <div className='navbar-right flex-1 flex justify-end items-center gap-10 '>
           <Image
             src={assets.search_icon}
             alt='Search icon'
@@ -43,7 +53,7 @@ const Navbar = () => {
             height={24}
             className='cursor-pointer'
           />
-          <div className='navbar-search-icon flex items-center space-x-4'>
+          <div className='navbar-search-icon flex items-center space-x-4 relative'>
             <Image
               src={assets.basket_icon}
               alt='basket icon'
@@ -51,8 +61,8 @@ const Navbar = () => {
               height={24}
               className='cursor-pointer'
             />
-            <div className='dot'></div>
-            <button className='px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition-colors'>
+            <div className='dot absolute min-w-2.5 min-h-2.5 bg-orange-500 rounded-md top-[-8px] left-3'></div>
+            <button className='px-4 py-2  text-gray-800 rounded-[20px] border border-orange-500 hover:bg-orange-500 hover:text-white transition-colors'>
               Sign in
             </button>
           </div>
