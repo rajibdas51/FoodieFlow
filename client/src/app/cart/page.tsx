bfab839c5f6e85ffa91d4cff7a474e9d9be67716
Rajib <prorajib6@gmail.com>
2025-03-18 23:20:02 +0600

    update cart page

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,17 +1,27 @@
 'use client';
-import { RootState } from '@/redux/store';
+
 import Image from 'next/image';
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeFromCart } from '@/redux/slices/cartSlice';
+import { useCart } from '@/hooks/useCart';
+import { RootState } from '@/redux/store';
+
 const CartPage = () => {
-  const { cartItems } = useSelector((state: RootState) => state.cart);
-  const { foodList } = useSelector((state: RootState) => state.food);
   const dispatch = useDispatch();
+  const { cartProducts, subtotal, total, itemCount, deliveryFee, isEmpty } =
+    useCart();
+  const { cartItems } = useSelector((state: RootState) => state.cart);
+  // Handle item removal
+  const handleRemoveItem = (itemId: string) => {
+    dispatch(removeFromCart(itemId));
+  };
+
   return (
     <div className='container mx-auto'>
       <div className='cart mt-[100px]'>
+        {/* Cart Header */}
         <div className='cart-items px-1'>
           <div className='cart-items-title grid grid-cols-6 gap-4 items-center text-center text-gray-600 font-semibold'>
             <p>Items</p>
@@ -23,78 +33,91 @@ const CartPage = () => {
           </div>
           <br />
           <hr />
-          {foodList.map((item, index) => {
-            {
-              if (cartItems[item._id] > 0) {
-                return (
-                  <div>
-                    <div
-                      className='cart-items-title cart-items-item cart-items-title grid grid-cols-6 gap-4 items-center text-center text-gray-600 font-semibold mb-3  justify-center my-2'
-                      key={index}
-                    >
-                      <Image
-                        src={item.image}
-                        width={100}
-                        height={100}
-                        alt={item.name}
-                        className='rounded-sm'
-                      />
-                      <p>{item.name}</p>
-                      <p>${item.price}</p>
-                      <p>{cartItems[item._id]}</p>
-                      <p>{item.price * cartItems[item._id]}</p>
-                      <p className='text-2l'>
-                        <FaTimes
-                          onClick={() => dispatch(removeFromCart(item._id))}
-                          className='text-orange-600 text-xl cursor-pointer'
-                        />{' '}
-                      </p>
-                    </div>
-                    <hr />
-                  </div>
-                );
-              }
-            }
-          })}
+
+          {/* Cart Items or Empty Message */}
+          {isEmpty ? (
+            <div className='text-center py-10'>
+              <p className='text-gray-500'>Your cart is empty</p>
+            </div>
+          ) : (
+            cartProducts.map((item) => (
+              <div key={item._id}>
+                <div className='grid grid-cols-6 gap-4 items-center text-center text-gray-600 font-semibold mb-3 justify-center my-2'>
+                  <Image
+                    src={item.image}
+                    width={100}
+                    height={100}
+                    alt={item.name}
+                    className='rounded-sm'
+                  />
+                  <p>{item.name}</p>
+                  <p>${item.price.toFixed(2)}</p>
+                  <p>{item._id in cartItems ? cartItems[item._id] : 0}</p>
+                  <p>
+                    $
+                    {(
+                      (item._id in cartItems ? cartItems[item._id] : 0) *
+                      item.price
+                    ).toFixed(2)}
+                  </p>
+                  <p className='text-2l'>
+                    <FaTimes
+                      onClick={() => handleRemoveItem(item._id)}
+                      className='text-orange-600 text-xl cursor-pointer'
+                    />
+                  </p>
+                </div>
+                <hr />
+              </div>
+            ))
+          )}
         </div>
-        {/*-------cart bottom--------*/}
-        <div className='mt-[80px] flex  flex-col-reverse md:flex-row  justify-between gap-20  mb-6 lg:mb-10'>
+
+        {/* Cart Summary */}
+        <div className='mt-[80px] flex flex-col-reverse md:flex-row justify-between gap-20 mb-6 lg:mb-10'>
           <div className='w-full md:w-2/3 flex flex-col gap-5 px-2 md:px-2'>
             <h2 className='text-2xl font-bold'>Cart Totals</h2>
             <div className='flex flex-col gap-4'>
               <div className='flex justify-between text-gray-600'>
                 <p>Subtotal</p>
-                <p>{0}</p>
+                <p>${subtotal.toFixed(2)}</p>
               </div>
               <hr />
               <div className='flex justify-between text-gray-600'>
                 <p>Delivery Fee</p>
-                <p>{2}</p>
+                <p>${deliveryFee.toFixed(2)}</p>
               </div>
               <hr />
 
-              <div className='flex justify-between text-gray-600'>
+              <div className='flex justify-between text-gray-600 font-bold'>
                 <p>Total</p>
-                <p>{0}</p>
+                <p>${total.toFixed(2)}</p>
               </div>
               <hr />
 
-              <button className='text-white bg-orange-500 w-[200px] py-3 rounded-md cursor-pointer my-4'>
+              <button
+                className={`text-white bg-orange-500 w-[200px] py-3 rounded-md cursor-pointer my-4 ${
+                  isEmpty ? 'opacity-50 cursor-not-allowed' : ''
+                }`}
+                disabled={isEmpty}
+              >
                 PROCEED TO CHECKOUT
               </button>
             </div>
           </div>
-          <div className=' w-full md:w-1/3 flex flex-col md:items-center px-2'>
+
+          {/* Promo Code Section */}
+          <div className='w-full md:w-1/3 flex flex-col md:items-center px-2'>
             <p className='text-gray-600 text-lg'>
               If you have any promo code, Enter it here
             </p>
             <div className='mt-3 flex flex-row justify-between items-center bg-[#eaeaea] rounded-md'>
               <input
-                className='bg-transparent outline-none pl-3 '
+                className='bg-transparent outline-none pl-3 w-full py-3'
                 type='text'
                 placeholder='Promo Code'
               />
-              <button className='w-[150px] py-3 px-[5px] cursor-pointer  bg-black text-white text font-bold rounded-2xl '>
+              <button className='w-[150px] py-3 px-[5px] cursor-pointer bg-black text-white font-bold rounded-2xl'>
                 Submit
               </button>
             </div>
