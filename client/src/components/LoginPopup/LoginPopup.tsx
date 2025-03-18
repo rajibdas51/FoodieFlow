b80f3cd65d5690ed1f25df6fcc4e52160c13d4b1
Rajib <prorajib6@gmail.com>
2025-03-18 18:56:01 +0600

    update loginPopup

--- client/src/components/LoginPopup/LoginPopup.tsx
+++ client/src/components/LoginPopup/LoginPopup.tsx
@@ -1,24 +1,38 @@
 import { assets } from '@/assets/frontend_assets/assets';
 import Image from 'next/image';
-import React, { useState } from 'react';
-interface LoginPopupProps {
-  setShowLogin: (show: boolean) => void;
-}
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleAuthModal, setAuthModalView } from '@/redux/slices/authSlice';
+import { RootState } from '@/redux/store';
 
-const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
-  const [currentState, setCurrentState] = useState('Sign Up');
+const LoginPopup: React.FC = () => {
+  const dispatch = useDispatch();
+  const { authModalView } = useSelector((state: RootState) => state.auth);
+
+  const handleCloseModal = () => {
+    dispatch(toggleAuthModal(false));
+  };
+
+  const switchToLogin = () => {
+    dispatch(setAuthModalView('login'));
+  };
+
+  const switchToSignup = () => {
+    dispatch(setAuthModalView('signup'));
+  };
+
   return (
-    <div className='fixed inset-0  bg-[#00000090] flex w-full h-full z-[999]'>
+    <div className='login-popup fixed inset-0 bg-[#00000090] flex w-full h-full z-[100]'>
       <form
         action=''
-        className=' bg-white rounded-md place-self-center items-center justify-center mx-auto w-[330px] text-[#808080] flex flex-col gap-6 py-8 px-3 relative animate-fade-in'
+        className='login-popup-container bg-white rounded-md place-self-center items-center justify-center mx-auto w-[330px] text-[#808080] flex flex-col gap-6 py-8 px-3 relative animate-fade-in'
       >
         <div className='login-popup-title'>
           <h2 className='text-2xl font-semibold text-gray-800'>
-            {currentState}
+            {authModalView === 'signup' ? 'Sign Up' : 'Login'}
           </h2>
           <Image
-            onClick={() => setShowLogin(false)}
+            onClick={handleCloseModal}
             width={100}
             height={100}
             src={assets.cross_icon}
@@ -26,8 +40,8 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
             className='absolute top-3 right-3 w-5 cursor-pointer'
           />
         </div>
-        <div className='w-full flex flex-col  items-center justify-center mx-auto gap-6 '>
-          {currentState === 'Sign Up' && (
+        <div className='w-full flex flex-col items-center justify-center mx-auto gap-6'>
+          {authModalView === 'signup' && (
             <input
               type='text'
               name='name'
@@ -37,7 +51,6 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
               required
             />
           )}
-
           <input
             type='email'
             name='email'
@@ -54,7 +67,7 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
             required
             className='w-6/7 mt-1 border border-gray-300 rounded-md p-2 text-sm focus:outline-none focus:ring-1 focus:ring-orange-500'
           />
-          {currentState === 'Sign Up' && (
+          {authModalView === 'signup' && (
             <input
               type='phone'
               name='phone'
@@ -65,25 +78,23 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
             />
           )}
           <button className='bg-orange-500 text-white py-2 px-4 rounded-md cursor-pointer'>
-            {' '}
-            {currentState === 'Sign Up' ? 'Create Account' : 'Login'}
+            {authModalView === 'signup' ? 'Create Account' : 'Login'}
           </button>
         </div>
-
-        <div className='login-popup-condition flex flex-col gap-3  items-center justify-center mx-auto'>
+        <div className='login-popup-condition flex flex-col gap-3 items-center justify-center mx-auto'>
           <div className='flex flex-row gap-3'>
             <input type='checkbox' required className='accent-orange-500' />
             <p className=''>
               By continuing, I agree to the terms of use & privacy policy.
             </p>
           </div>
-
-          {currentState === 'Sign Up' ? (
+          {authModalView === 'signup' ? (
             <div className='flex md:flex-row gap-3'>
               <p>Already have an account? </p>
               <button
+                type='button'
                 className='font-semibold text-orange-500 cursor-pointer'
-                onClick={() => setCurrentState('Login')}
+                onClick={switchToLogin}
               >
                 {'    '} Login here
               </button>
@@ -92,8 +103,9 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ setShowLogin }) => {
             <div className='flex md:flex-row gap-3'>
               <p>Don&apos;t have an account? </p>
               <button
+                type='button'
                 className='font-semibold text-orange-500 cursor-pointer'
-                onClick={() => setCurrentState('Sign Up')}
+                onClick={switchToSignup}
               >
                 {'    '} Sign Up here
               </button>
