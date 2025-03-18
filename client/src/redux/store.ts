bf571a5513be321c692086f675bdd87d464aa0cc
Rajib <prorajib6@gmail.com>
2025-03-18 17:50:23 +0600

    update store with authslice

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import foodSlice from './slices/foodSlice';
 import loaderSlice from './slices/loaderSlice';
 import cartSlice from './slices/cartSlice';
+import authSlice from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
     food: foodSlice,
     loader: loaderSlice,
     cart: cartSlice,
+    auth: authSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
