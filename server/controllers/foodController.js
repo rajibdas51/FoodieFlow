a8e77c58219f4d11e182c91994fbca08b8d48339
Rajib <prorajib6@gmail.com>
2025-03-25 00:31:49 +0600

    add listfood controller

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -21,6 +21,14 @@ const addFood = async (req, res) => {
 };
 
 // get food list
-const listFood = async (req, res) => {};
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Error' });
+  }
+};
 
 export { addFood, listFood };
