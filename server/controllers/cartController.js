4735dcf546b7eff098e2bb42eab4ff6d09085d5a
Rajib <prorajib6@gmail.com>
2025-05-13 00:01:27 +0600

    create addtocart controller

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,7 +1,22 @@
 import userModel from '../models/userModel';
 
 // add items to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findOne({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: 'Added to cart' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Error occured during add to cart!' });
+  }
+};
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {};
