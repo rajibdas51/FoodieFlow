89cbb0d1c663d9ea6c96d085d56782a263f9d6f0
Rajib <prorajib6@gmail.com>
2025-06-01 23:58:47 +0600

    create ordercontroller

---
+++ server/controllers/orderController.js
@@ -0,0 +1,9 @@
+import orderModel from '../models/orderModel';
+import userModel from '../models/userModel';
+import stripe from 'stripe';
+
+// Placing user order for frontend
+
+const placeOrder = async (req, res) => {};
+
+export { placeOrder };
