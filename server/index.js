adbd60356986cfdb702d2e899b13276af2fce0bf
Rajib <prorajib6@gmail.com>
2025-06-14 00:00:05 +0600

    server msg

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 dotenv.config();
 
 // app config
@@ -30,11 +31,11 @@ app.use('/api/food', foodRouter);
 app.use('/images', express.static('uploads'));
 app.use('/api/users', userRouter);
 app.use('/api/cart', cartRouter);
-
+app.use('/api/order', orderRouter);
 app.get('/', (req, res) => {
   res.send('API working.');
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
